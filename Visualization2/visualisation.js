3e88ae0a672da7f7c345ec9a2a200d11c5340c59
Joseph <44628818+josepht777@users.noreply.github.com>
2024-10-11 12:22:42 +1100

    Add files via upload

---
+++ Visualization2/visualisation.js
@@ -0,0 +1,10 @@
+var vg_1 = "PotatoChart.json";
+vegaEmbed("#potato", vg_1).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "CattleMap.json";
+vegaEmbed("#cattle", vg_2, {"actions": false}).then(function(result){}).catch(console.error);
+
+var vg_3 = "Dairyline.json";
+vegaEmbed("#dairy", vg_3, {"actions": false}).then(function(result){}).catch(console.error);
